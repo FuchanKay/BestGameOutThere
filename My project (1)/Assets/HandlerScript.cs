5fc4e23c3a4b88bbc865ce9229d07b76e0aae714
FuchanKay <kazeuma0923@gmail.com>
2025-02-16 18:30:04 -0500

    Created handler and entity handler

--- My project (1)/Assets/HandlerScript.cs
+++ My project (1)/Assets/HandlerScript.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class HandlerScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public const int moveDistance = 1;
+    public GameObject player;
     void Start()
     {
         
@@ -11,6 +13,18 @@ public class HandlerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 moveDirection = Vector3.zero;
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            moveDirection = Vector3.up;
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            moveDirection = Vector3.down;
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            moveDirection = Vector3.left;
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            moveDirection = Vector3.right;
+
+        Vector3 newPosition = transform.position + moveDirection * moveDistance;
+        transform.position = newPosition;
     }
 }
