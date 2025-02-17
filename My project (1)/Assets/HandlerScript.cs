5d5b40f675d8faebc46cae33f3156d81412d014a
FuchanKay <kazeuma0923@gmail.com>
2025-02-16 23:22:57 -0500

    Added some pawn functionalities

--- My project (1)/Assets/HandlerScript.cs
+++ My project (1)/Assets/HandlerScript.cs
@@ -5,26 +5,49 @@ public class HandlerScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public const int moveDistance = 1;
     public GameObject player;
+    public GameObject rookHandler;
+    public GameObject bishopHandler;
+    public GameObject pawnHandler;
+    public GameObject knightHandler;
+    public GameObject queenHandler;
+    public GameObject kingHandler;
+    public GameObject wallHandler;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 moveDirection = Vector3.zero;
-
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+        }
             moveDirection = Vector3.up;
         if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+
+        }
             moveDirection = Vector3.down;
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+        }
             moveDirection = Vector3.left;
         if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+        }
             moveDirection = Vector3.right;
+        if (Input.GetKeyDown(KeyCode.Space)) {
+
+        }
+
 
         Vector3 newPosition = transform.position + moveDirection * moveDistance;
-        transform.position = newPosition;
+        player.transform.position = newPosition;
     }
 }
---
+++ My project (1)/Assets/PawnBehavior.cs
@@ -0,0 +1,19 @@
+using UnityEditor.U2D.Animation;
+using UnityEngine;
+using UnityEngine.U2D.Animation;
+
+public class PawnBehavior : MonoBehaviour
+{
+    public SpriteLibrary lib;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
