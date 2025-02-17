08149261a031affd59a19a6ea59a798a0a2fae56
FuchanKay <kazeuma0923@gmail.com>
2025-02-17 13:05:33 -0500

    Added Pawn Animation

--- My project (1)/Assets/HandlerScript.cs
+++ My project (1)/Assets/HandlerScript.cs
@@ -4,6 +4,7 @@ public class HandlerScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public const int moveDistance = 1;
+    private float location = 2;
     public GameObject player;
     public GameObject rookHandler;
     public GameObject bishopHandler;
@@ -21,33 +22,15 @@ public class HandlerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 moveDirection = Vector3.zero;
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.T))
         {
-
+            pawnHandler.transform.GetChild(0).GetComponent<Animator>().Play("pawn_activation");
+            Debug.Log("asdkjfhl");
         }
-            moveDirection = Vector3.up;
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.S))
         {
+            
 
         }
-            moveDirection = Vector3.down;
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-
-        }
-            moveDirection = Vector3.left;
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-
-        }
-            moveDirection = Vector3.right;
-        if (Input.GetKeyDown(KeyCode.Space)) {
-
-        }
-
-
-        Vector3 newPosition = transform.position + moveDirection * moveDistance;
-        player.transform.position = newPosition;
     }
 }
