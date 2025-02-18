0bfee13a6c8e4fb187a1f29a1b5b21a86b9a5082
FuchanKay <kazeuma0923@gmail.com>
2025-02-17 22:42:05 -0500

    updated tile asset
    kinda unnecessary but i wanted to do it

--- My project (1)/Assets/Tiles/Tile.cs
+++ My project (1)/Assets/Tiles/Tile.cs
@@ -5,9 +5,12 @@ public class Tile : MonoBehaviour
 {
     [SerializeField] private Sprite _whiteTile, _blackTile;
     [SerializeField] private SpriteRenderer _renderer;
+    
     public void Init(bool isOffSet)
     {
+        int rotation = Random.Range(0, 4);
         _renderer.sprite = _whiteTile;
         _renderer.sprite = isOffSet ? _whiteTile : _blackTile;
+        _renderer.transform.rotation = Quaternion.Euler(0, 0, 90 * rotation);
     }
 }
